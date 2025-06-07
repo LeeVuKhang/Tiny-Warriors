058d877734589162373b4acf2c6cc7ad768f5561
LeeVuKhang <lvukhangpq@gmail.com>
2025-06-08 05:48:32 +0700

    map decoration + player movement

--- Assets/_GAME_/Scripts/PlayerMovement.cs
+++ Assets/_GAME_/Scripts/PlayerMovement.cs
@@ -1,15 +1,39 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
     public Rigidbody2D rb;
+    public int facingDirection = 1;
+
+    public Animator anim;
 
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        rb.linearVelocity = new Vector2(horizontal, vertical);
+        Debug.Log($"Input: {horizontal}, {vertical}"); // Debug xem có nhận input không
+
+        if ((horizontal > 0 && transform.localScale.x < 0) ||
+            (horizontal < 0 && transform.localScale.x > 0))
+        {
+            Flip();
+        }
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(
+            transform.localScale.x * -1,
+            transform.localScale.y,
+            transform.localScale.z
+        );
     }
 }
