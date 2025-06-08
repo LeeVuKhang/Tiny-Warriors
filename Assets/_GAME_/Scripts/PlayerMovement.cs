e4646d5052db54e1830e5ebac0c84c08e0fc81da
LeeVuKhang <lvukhangpq@gmail.com>
2025-06-09 00:06:55 +0700

    debug read-only player

--- Assets/_GAME_/Scripts/PlayerMovement.cs
+++ Assets/_GAME_/Scripts/PlayerMovement.cs
@@ -5,26 +5,30 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5;
     public Rigidbody2D rb;
     public int facingDirection = 1;
-
     public Animator anim;
 
-    private void Update()
+    private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        Debug.Log($"Input: {horizontal}, {vertical}");
 
-        Debug.Log($"Input: {horizontal}, {vertical}"); // Debug xem có nhận input không
-
+        // Flip logic
         if ((horizontal > 0 && transform.localScale.x < 0) ||
             (horizontal < 0 && transform.localScale.x > 0))
         {
             Flip();
         }
 
+        // Animation parameters
         anim.SetFloat("horizontal", Mathf.Abs(horizontal));
         anim.SetFloat("vertical", Mathf.Abs(vertical));
 
-        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+        Vector2 inputVector = new Vector2(horizontal, vertical);
+
+        Vector2 move = inputVector.magnitude > 0.1f ? inputVector.normalized : Vector2.zero;
+
+        rb.linearVelocity = move * speed;
     }
 
     void Flip()
